e553c8838b91a31bd017c16f535823088451343a
Manarah team <79666988+ManarahApp@users.noreply.github.com>
2021-10-09 21:18:44 +0300

    CalculatorApp

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 
 /*
 Build a single activity calculator application
@@ -28,112 +29,112 @@ class MainActivity : AppCompatActivity() {
     var num1 = ""
     var num2 = ""
 
-    var displayText = "0"
+    //var displayText = "0"
 
     //numbers
-    private lateinit var zero: Button
-    private lateinit var one: Button
-    private lateinit var two: Button
-    private lateinit var three: Button
-    private lateinit var four: Button
-    private lateinit var five: Button
-    private lateinit var six: Button
-    private lateinit var seven: Button
-    private lateinit var eight: Button
-    private lateinit var nine: Button
+    private lateinit var Zero: Button
+    private lateinit var One: Button
+    private lateinit var Two: Button
+    private lateinit var Three: Button
+    private lateinit var Four: Button
+    private lateinit var Five: Button
+    private lateinit var Six: Button
+    private lateinit var Seven: Button
+    private lateinit var Eight: Button
+    private lateinit var Nine: Button
 
     //operations
-    private lateinit var plusMinus: Button
-    private lateinit var decimal: Button
-    private lateinit var multiply: Button
-    private lateinit var divide: Button
-    private lateinit var add: Button
-    private lateinit var subtract: Button
-    private lateinit var clear: Button
-    private lateinit var result: Button
-    private lateinit var del: Button
+    private lateinit var PlusMinus: Button
+    private lateinit var Decimal: Button
+    private lateinit var Multiplication: Button
+    private lateinit var Dividtion: Button
+    private lateinit var Addtion: Button
+    private lateinit var Subtraction: Button
+    private lateinit var ClearResults: Button
+    private lateinit var Result: Button
+    private lateinit var Delete: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //others initializing
-        display = findViewById(R.id.tvDisplay)
-
+        display = findViewById(R.id.DisplayResualtTV)
+        display.hint = "0"
         //numbers initializing
-        zero = findViewById(R.id.bt0)
-        zero.setOnClickListener { setNum('0') }
-        one = findViewById(R.id.bt1)
-        one.setOnClickListener { setNum('1') }
-        two = findViewById(R.id.bt2)
-        two.setOnClickListener { setNum('2') }
-        three = findViewById(R.id.bt3)
-        three.setOnClickListener { setNum('3') }
-        four = findViewById(R.id.bt4)
-        four.setOnClickListener { setNum('4') }
-        five = findViewById(R.id.bt5)
-        five.setOnClickListener { setNum('5') }
-        six = findViewById(R.id.bt6)
-        six.setOnClickListener { setNum('6') }
-        seven = findViewById(R.id.bt7)
-        seven.setOnClickListener { setNum('7') }
-        eight = findViewById(R.id.bt8)
-        eight.setOnClickListener { setNum('8') }
-        nine = findViewById(R.id.bt9)
-        nine.setOnClickListener { setNum('9') }
+        Zero = findViewById(R.id.btn0)
+        Zero.setOnClickListener { SetNumber('0') }
+        One = findViewById(R.id.btn1)
+        One.setOnClickListener { SetNumber('1') }
+        Two = findViewById(R.id.btn2)
+        Two.setOnClickListener { SetNumber('2') }
+        Three = findViewById(R.id.btn3)
+        Three.setOnClickListener { SetNumber('3') }
+        Four = findViewById(R.id.btn4)
+        Four.setOnClickListener { SetNumber('4') }
+        Five = findViewById(R.id.btn5)
+        Five.setOnClickListener { SetNumber('5') }
+        Six = findViewById(R.id.btn6)
+        Six.setOnClickListener { SetNumber('6') }
+        Seven = findViewById(R.id.btn7)
+        Seven.setOnClickListener { SetNumber('7') }
+        Eight = findViewById(R.id.btn8)
+        Eight.setOnClickListener { SetNumber('8') }
+        Nine = findViewById(R.id.btn9)
+        Nine.setOnClickListener { SetNumber('9') }
 
         //operations initializing
-        add = findViewById(R.id.btPlus)
-        add.setOnClickListener { handleOperator('+') }
-        subtract = findViewById(R.id.btMinus)
-        subtract.setOnClickListener { handleOperator('-') }
-        multiply = findViewById(R.id.btMultiply)
-        multiply.setOnClickListener { handleOperator('*') }
-        divide = findViewById(R.id.btDiv)
-        divide.setOnClickListener { handleOperator('/') }
-        decimal = findViewById(R.id.btDecimal)
-        decimal.setOnClickListener { onClickDecimal() }
-        plusMinus = findViewById(R.id.btNegative)
-        plusMinus.setOnClickListener { onClickPlusMinus() }
-        clear = findViewById(R.id.btClear)
-        clear.setOnClickListener { clearAll() }
-        result = findViewById(R.id.btEquals)
-        result.setOnClickListener { calculate() }
-        del = findViewById(R.id.btDel)
-        del.setOnClickListener { deleteLast() }
+        Addtion = findViewById(R.id.Plusbtn)
+        Addtion.setOnClickListener { SetOperator('+') }
+        Subtraction = findViewById(R.id.Minusbtn)
+        Subtraction.setOnClickListener { SetOperator('-') }
+        Multiplication = findViewById(R.id.Multiplybtn)
+        Multiplication.setOnClickListener { SetOperator('*') }
+        Dividtion = findViewById(R.id.Dividebtn)
+        Dividtion.setOnClickListener { SetOperator('/') }
+        Decimal = findViewById(R.id.Decimalbtn)
+        Decimal.setOnClickListener { DecimalClicked() }
+        PlusMinus = findViewById(R.id.NegativePositivebtn)
+        PlusMinus.setOnClickListener { PlusMinusClicked() }
+        ClearResults = findViewById(R.id.Clearbtn)
+        ClearResults.setOnClickListener { ClearResualts() }
+        Result = findViewById(R.id.Equalbtn)
+        Result.setOnClickListener { Calculate() }
+        Delete = findViewById(R.id.Deletebtn)
+        Delete.setOnClickListener { Delete() }
 
         title = "Calculator"
     }//end oncreate
 
-    fun setNum(num: Char){
+    fun SetNumber(num: Char){
         if(operator==' '){
             num1 += num
-            displayText = num1
+            display.text = num1
         }else{
             num2 += num
             val text = num1 + operator + num2
-            displayText = text
+            display.text = text
         }
     }//end setNum
 
-    fun handleOperator(op: Char){
+    fun SetOperator(op: Char){
         operator = op
         val text = num1 + operator
-        displayText = text
+        display.text = text
     }//end handelOperator
 
-    fun onClickDecimal(){
-        if(operator==' '&&!num1.contains(".")){setNum('.')}
-        if(operator!=' '&&!num2.contains(".")){setNum('.')}
+    fun DecimalClicked(){
+        if(operator==' '&&!num1.contains(".")){SetNumber('.')}
+        if(operator!=' '&&!num2.contains(".")){SetNumber('.')}
     }//end onClickDecimal
 
-    fun onClickPlusMinus(){
+    fun PlusMinusClicked(){
         if(operator==' '){
             num1 = if(num1.startsWith("-")){
                 num1.substring(1, num1.length)
             } else{
                 "-$num1"
             }
-            displayText = num1
+            display.text = num1
         }else{
             num2 = if(num2.startsWith("-")){
                 num2.substring(1, num2.length)
@@ -141,19 +142,19 @@ class MainActivity : AppCompatActivity() {
                 "-$num2"
             }
             val text = num1 + operator + num2
-            displayText = text
+            display.text = text
         }
     }//end onClickPlusMinus
 
-    fun clearAll(){
+    fun ClearResualts(){
         output = 0f
         operator = ' '
         num1 = ""
         num2 = ""
-        displayText = "0"
+        display.text = "0"
     }//end clearAll
 
-    fun calculate(){
+    fun Calculate(){
         var divByZero = false
         when (operator) {
             '+' -> output = num1.toFloat() + num2.toFloat()
@@ -163,29 +164,35 @@ class MainActivity : AppCompatActivity() {
                 output = num1.toFloat() / num2.toFloat()
             }else{
                 divByZero = true
+                //Snackbar.make(clo, "You cant divide by zero!", Snackbar.LENGTH_LONG).show()
+
             }
         }
         num1 = output.toString()
         num2 = ""
-        displayText = output.toString()
-        if(divByZero){clearAll()}
+        display.text = output.toString()
+        if(divByZero){
+            ClearResualts()
+        }
     }//end calculate
 
-    fun deleteLast(){
+    fun Delete(){
         if(operator==' '){
             if(num1.isNotEmpty()){
                 num1 = num1.substring(0, num1.length - 1)
-                if(num1.isEmpty()){displayText = "0"}
-                else{displayText = num1}
+                if(num1.isEmpty()){display.text = "0"}
+                else{
+                    display.text = num1
+                }
             }
         }else{
             if(num2.isNotEmpty()){
                 num2 = num2.substring(0, num2.length - 1)
                 val text = num1 + operator + num2
-                displayText = text
+                display.text = text
             }else{
                 operator=' '
-                displayText = num1
+                display.text = num1
             }
         }
     }//end deleteLast
